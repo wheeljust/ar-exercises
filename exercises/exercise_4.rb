4407c640fa36c801e507dd5f1a6b4e069fd8415e
Justin Wheeler <justinwwheeler@gmail.com>
2022-01-07 04:43:28 +0000

    load subset of stores carrying mens and womens clothing

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Fetch stores that sell only mens
+@mens_stores = Store.where(mens_apparel: true)
+
+# Fetch stores selling womens and having revenue less that 1mill
+@womens_stores = Store.where(["womens_apparel = ? and annual_revenue < ?", true, 1000000])
+
+
+puts "--- LIST OF MENS STORES --- "
+@mens_stores.each do |store|
+  puts store.name
+end
+
+puts "--- LIST OF WOMENS STORES HAVING < $1MM REVENUE --- "
+@womens_stores.each do |store|
+  puts store.name
+end
