f179c4fb5c515f117b18c17ce0acc460e67eaf3e
Justin Wheeler <justinwwheeler@gmail.com>
2022-01-07 04:56:58 +0000

    calcuate some store KPIs

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,18 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Total revenue 
+
+@total_revenue = Store.sum(:annual_revenue)
+puts "The total annual revenue of all stores combined is $#{@total_revenue}"
+
+# Avg revenue for all stores
+
+@average_revenue = Store.average(:annual_revenue)
+puts "The average annual revenue of all stores is $#{@average_revenue}"
+
+# number of stores earning over $1MM
+
+@num_of_big_stores = Store.where("annual_revenue > 1000000").count
+puts "There are #{@num_of_big_stores} stores earning more than $1MM in annual revenue"
